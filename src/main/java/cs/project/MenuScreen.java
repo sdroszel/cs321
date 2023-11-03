6f833a65a135a38220780cdf34f288bdca08fa87
Scott Roszel <57762591+sdroszel@users.noreply.github.com>
2023-11-03 11:06:19 -0400

    Added title to menu screen

--- src/main/java/cs/project/MenuScreen.java
+++ src/main/java/cs/project/MenuScreen.java
@@ -1,12 +1,16 @@
 package cs.project;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Background;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class MenuScreen extends Application {
@@ -20,6 +24,19 @@ public class MenuScreen extends Application {
         Stage stage;
         stage = primaryStage;
         primaryStage.setTitle("Main Menu");
+
+        Text title = new Text("USCIS Employee Portal");
+        title.setFont(Font.font("Arial", 16));
+
+        // setup grid to hold labels and text-fields
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.BASELINE_CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+        grid.setBackground(Background.fill(Color.IVORY));
+
+        grid.add(title, 0, 0, 3, 1);
 
         Button dataEntryButton = new Button("Data Entry");
         Button reviewButton = new Button("Review");
@@ -35,9 +52,9 @@ public class MenuScreen extends Application {
         buttonBox.setAlignment(Pos.CENTER);
         buttonBox.getChildren().addAll(dataEntryButton, reviewButton, approvalButton);
 
-        buttonBox.setBackground(Background.fill(Color.IVORY));
+        grid.add(buttonBox, 0, 2);
 
-        Scene scene = new Scene(buttonBox, 300, 100);
+        Scene scene = new Scene(grid, 300, 100);
         primaryStage.setScene(scene);
         stage.show();
     }
